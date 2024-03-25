2aa3c2e23481391b50323014bcba733c9900acdc
feraistanti <fistanti551@gmail.com>
2024-03-25 15:09:01 +0700

    menamnbahkan case

--- 083_FungsiSwitch/083_FungsiSwitch.cpp
+++ 083_FungsiSwitch/083_FungsiSwitch.cpp
@@ -43,6 +43,34 @@ int main()
         cout << "1, penjumlahan " << endl;
         cout << "2, pembagian " << endl;
         cout << "3, perkalian " << endl;
-        cout << "4 , pembagian " << endl;
-    }
-}
+        cout << "4, pembagian " << endl;
+        cout << "5, Exit" << endl;
+        cout << "Masukan Pilihan 1/2/3/4/5 = ";
+        cin >> pilihan;
+    
+        switch (pilihan)
+        {
+        case 1:
+            input();
+            cout << "Hasil penjumlahan = " << penjumlahan(bilSatu, bilDua) << endl;
+            break;
+        case 2:
+            input();
+            cout << "Hasil pembagian = " << pembagian(bilSatu, bilDua) << endl;
+            break;
+        case 3:
+            input();
+            cout << "Hasil perkalian = " << perkalian(bilSatu, bilDua) << endl;
+            break;
+        case 4:
+            input();
+            cout << "Hasil pengurangan = " << pengurangan(bilSatu, bilDua) << endl;
+            break;
+        case 5:
+            break;
+        default:
+            cout << "pilihan tidak ada" << endl;
+            break;
+            system("CLS");
+        }
+    }while (pilihan != 5);
